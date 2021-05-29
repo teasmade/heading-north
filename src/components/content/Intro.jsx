9aba45a0e3baca9299dc9fe849c5dab7e8279582
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-05-29 09:00:11 +0200

    adjust svg scaling, add media queries, resolve sticky header

--- src/components/content/Intro.jsx
+++ src/components/content/Intro.jsx
@@ -4,19 +4,22 @@ function Intro() {
   return (
     <div>
       <div className={css.intro}>
+        <h1>Thomas Allen</h1>
         <h2>Web Developer</h2>
-        <p className={css.paraBlock}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi aspernatur dolores odit
-          dolor sit amet consectetur.
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, quia. Sunt recusandae
-          ipsa.
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, quia. Sunt recusandae
-          ipsa.
-        </p>
+        <div className={css.paraBlock}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi aspernatur dolores
+            odit dolor sit amet consectetur.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, quia. Sunt
+            recusandae ipsa.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, quia. Sunt
+            recusandae ipsa.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -3,7 +3,6 @@ import css from './Nav.module.css';
 function Nav() {
   return (
     <nav className={css.nav}>
-      <h1 className={css.title}>Thomas Allen</h1>
       <span className={css.home}>H</span>
       <span className={css.lang}>En / Fr</span>
       <span className={css.burger}>///</span>
