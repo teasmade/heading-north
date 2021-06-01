7ece4c367deaf2fef7b3fa58b807d14eafaa5938
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-06-01 12:58:14 +0200

    viewport scaling for larger desktops

--- src/components/content/TimeLine.jsx
+++ src/components/content/TimeLine.jsx
@@ -6,97 +6,104 @@ import css from './TimeLine.module.css';
 function TimeLine() {
   return (
     <div className={css.timeline}>
-      <h2>Recent Work & Projects</h2>
-      <VerticalTimeline className={css.timeLineTrunk} animate={false} layout='1-column-right'>
-        <VerticalTimelineElement
-          className='vertical-timeline-element--work'
-          contentStyle={{ background: '#7cccf566' }}
-          date='March - July 2021'
-          iconStyle={{ background: '#ddffff', color: '#ddffff' }}
-          iconClassName={css.timeLineNode}
-          textClassName={css.timeLineBox}
-          dateClassName={css.timeLineDate}
-          // icon={<WorkIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'>Developer-in-training</h3>
-          <h4 className='vertical-timeline-element-subtitle'>Wild Code School, Nantes</h4>
-          <p>
-            Full time training in front and back end web development using React.js, Node.js, MySQL
-            and Express.
-          </p>
-          <p>
-            Emphasis on the importance of Agile work practices, version control using Git and
-            Github, quality control using ESLint.
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className='vertical-timeline-element--work'
-          contentStyle={{ background: '#7cccf566' }}
-          date='May - July 2021'
-          iconStyle={{ background: '#ddffff', color: '#ddffff' }}
-          iconClassName={css.timeLineNode}
-          textClassName={css.timeLineBox}
-          dateClassName={css.timeLineDate}
-          // icon={<WorkIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'>Pricing Control Application</h3>
-          <h4 className='vertical-timeline-element-subtitle'>www.plusdebad.com</h4>
-          <p>
-            My final project during training at the Wild Code School, Nantes, working for an
-            external client on a real-world problem in a team of 5.
-          </p>
-          <p>
-            Application to be delivered July 2021: a system automating product ticketing processes,
-            based on dynamic price and stock data, for a chain of 7 specialist sports shops.
-          </p>
-          <p>
-            Working in 1 week sprints, we will develop our own back-end, API and front-end, usinng
-            React, Node and MySQL.
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className='vertical-timeline-element--work'
-          contentStyle={{ background: '#7cccf566' }}
-          date='May 2021'
-          iconStyle={{ background: '#ddffff', color: '#ddffff' }}
-          iconClassName={css.timeLineNode}
-          textClassName={css.timeLineBox}
-          dateClassName={css.timeLineDate}
-          // icon={<WorkIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'>Hackathon Winning Entry</h3>
-          <h4 className='vertical-timeline-element-subtitle'>To the Moon!</h4>
-          <p>
-            I was part of a team of 4, winning a 2 day hackathon run during training at the Wild
-            Code School.
-          </p>
-          <p>We developed a playable space-adventure game, using React and p5.js.</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className='vertical-timeline-element--work'
-          contentStyle={{ background: '#7cccf566' }}
-          date='April - May 2021'
-          iconStyle={{ background: '#ddffff', color: '#ddffff' }}
-          iconClassName={css.timeLineNode}
-          textClassName={css.timeLineBox}
-          dateClassName={css.timeLineDate}
-          // icon={<WorkIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'>React + API Development</h3>
-          <h4 className='vertical-timeline-element-subtitle'>BiclooApp</h4>
-          <p>
-            2nd project during training at the Wild Code School; our brief was to create a site with
-            React using geodata from Nantes Metropôle's public API in an interesting way.
-          </p>
-          <p>
-            My team of 5 developed an app to gameify Bicloo, Nantes' on-demand bike rental service.
-          </p>
-          <p>
-            We used React alongside Leaflet for dynamic map data display. We exceeded the project
-            brief in the time allocated, adding authentication and user data storage using Firebase.
-          </p>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+      <div className={css.desktopFlexWrapper}>
+        <div className={css.desktopFlexItem}>
+          <h2>Recent Work & Projects</h2>
+          <VerticalTimeline className={css.timeLineTrunk} animate={false} layout='1-column-right'>
+            <VerticalTimelineElement
+              className='vertical-timeline-element--work'
+              contentStyle={{ background: '#7cccf566' }}
+              date='March - July 2021'
+              iconStyle={{ background: '#ddffff', color: '#ddffff' }}
+              iconClassName={css.timeLineNode}
+              textClassName={css.timeLineBox}
+              dateClassName={css.timeLineDate}
+              // icon={<WorkIcon />}
+            >
+              <h3 className='vertical-timeline-element-title'>Developer-in-training</h3>
+              <h4 className='vertical-timeline-element-subtitle'>Wild Code School, Nantes</h4>
+              <p>
+                Full time training in front and back end web development using React.js, Node.js,
+                MySQL and Express.
+              </p>
+              <p>
+                Emphasis on the importance of Agile work practices, version control using Git and
+                Github, quality control using ESLint.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className='vertical-timeline-element--work'
+              contentStyle={{ background: '#7cccf566' }}
+              date='May - July 2021'
+              iconStyle={{ background: '#ddffff', color: '#ddffff' }}
+              iconClassName={css.timeLineNode}
+              textClassName={css.timeLineBox}
+              dateClassName={css.timeLineDate}
+              // icon={<WorkIcon />}
+            >
+              <h3 className='vertical-timeline-element-title'>Pricing Control Application</h3>
+              <h4 className='vertical-timeline-element-subtitle'>www.plusdebad.com</h4>
+              <p>
+                My final project during training at the Wild Code School, Nantes, working for an
+                external client on a real-world problem in a team of 5.
+              </p>
+              <p>
+                Application to be delivered July 2021: a system automating product ticketing
+                processes, based on dynamic price and stock data, for a chain of 7 specialist sports
+                shops.
+              </p>
+              <p>
+                Working in 1 week sprints, we will develop our own back-end, API and front-end,
+                usinng React, Node and MySQL.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className='vertical-timeline-element--work'
+              contentStyle={{ background: '#7cccf566' }}
+              date='May 2021'
+              iconStyle={{ background: '#ddffff', color: '#ddffff' }}
+              iconClassName={css.timeLineNode}
+              textClassName={css.timeLineBox}
+              dateClassName={css.timeLineDate}
+              // icon={<WorkIcon />}
+            >
+              <h3 className='vertical-timeline-element-title'>Hackathon Winning Entry</h3>
+              <h4 className='vertical-timeline-element-subtitle'>To the Moon!</h4>
+              <p>
+                I was part of a team of 4, winning a 2 day hackathon run during training at the Wild
+                Code School.
+              </p>
+              <p>We developed a playable space-adventure game, using React and p5.js.</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className='vertical-timeline-element--work'
+              contentStyle={{ background: '#7cccf566' }}
+              date='April - May 2021'
+              iconStyle={{ background: '#ddffff', color: '#ddffff' }}
+              iconClassName={css.timeLineNode}
+              textClassName={css.timeLineBox}
+              dateClassName={css.timeLineDate}
+              // icon={<WorkIcon />}
+            >
+              <h3 className='vertical-timeline-element-title'>React + API Development</h3>
+              <h4 className='vertical-timeline-element-subtitle'>BiclooApp</h4>
+              <p>
+                2nd project during training at the Wild Code School; our brief was to create a site
+                with React using geodata from Nantes Metropôle's public API in an interesting way.
+              </p>
+              <p>
+                My team of 5 developed an app to gameify Bicloo, Nantes' on-demand bike rental
+                service.
+              </p>
+              <p>
+                We used React alongside Leaflet for dynamic map data display. We exceeded the
+                project brief in the time allocated, adding authentication and user data storage
+                using Firebase.
+              </p>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/content/TopSection.jsx
+++ src/components/content/TopSection.jsx
@@ -3,31 +3,36 @@ import css from './TopSection.module.css';
 function TopSection() {
   return (
     <div className={css.topSection}>
-      <h2>Right Here, Right Now</h2>
-      <p>
-        I'm a <span className={css.highlight}>junior developer</span> currently learning the trade
-        with <span className={css.highlight}>React, Node and MySQL.</span> My pre-dev background is
-        in sales and language teaching.{' '}
-      </p>
-      <p>
-        <span className={css.highlight}>Fluent,</span> targeted{' '}
-        <span className={css.highlight}>communication,</span> efficient{' '}
-        <span className={css.highlight}>organisation</span> and adaptation to new{' '}
-        <span className={css.highlight}>learning </span>
-        contexts are some of my prior <span className={css.highlight}>strengths.</span> I've found
-        they retain their value as this journey continues...
-      </p>
-      <p>
-        I'm based in <span className={css.highlight}>Nantes,</span> France, and am currently looking
-        for <span className={css.highlight}>interesting opportunities</span> in the region, or
-        remote CET, for <span className={css.highlight}>junior-level</span> roles or internships
-        starting from <span className={css.highlight}>August 2021.</span>
-      </p>
-      <p>
-        <span className={css.highlight}>// TODO:</span> my current learning goals are{' '}
-        <span className={css.highlight}>Typescript, GraphQL and Tailwind,</span> with side interests
-        in creative coding with <span className={css.highlight}>p5.js and D3.js.</span>
-      </p>
+      <div className={css.desktopFlexWrapper}>
+        <div className={css.desktopFlexItem}>
+          <h2>Right Here, Right Now</h2>
+          <p>
+            I'm a <span className={css.highlight}>junior developer</span> currently learning the
+            trade with <span className={css.highlight}>React, Node and MySQL.</span> My pre-dev
+            background is in sales and language teaching.{' '}
+          </p>
+          <p>
+            <span className={css.highlight}>Fluent,</span> targeted{' '}
+            <span className={css.highlight}>communication,</span> efficient{' '}
+            <span className={css.highlight}>organisation</span> and adaptation to new{' '}
+            <span className={css.highlight}>learning </span>
+            contexts are some of my prior <span className={css.highlight}>strengths.</span> I've
+            found they retain their value as this journey continues...
+          </p>
+          <p>
+            I'm based in <span className={css.highlight}>Nantes,</span> France, and am currently
+            looking for <span className={css.highlight}>interesting opportunities</span> in the
+            region, or remote CET, for <span className={css.highlight}>junior-level</span> roles or
+            internships starting from <span className={css.highlight}>August 2021.</span>
+          </p>
+          <p>
+            <span className={css.highlight}>// TODO:</span> my current learning goals are{' '}
+            <span className={css.highlight}>Typescript, GraphQL and Tailwind,</span> with side
+            interests in creative coding with{' '}
+            <span className={css.highlight}>p5.js and D3.js.</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
